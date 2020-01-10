d65d13e772223773df146773b2932aca3e0ac119
hariswb <hariswb@gmail.com>
2020-01-10 20:49:50 +0700

    Change README

--- 003largestPrimeFactors.py
+++ 003largestPrimeFactors.py
@@ -1,7 +1,7 @@
 # Problem: The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 # 
-# The algorithm only works with free square composite number such as the one the question asks 
+# The algorithm only works with squarefree composite number in which no prime factor repeats itself like 42 = 2 * 3 * 7, such as the one the question asks 
 
 def largestPrimeFactor(num):
     ## dividing the number until it's quotient is 1 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# projecteulersolutions
+# Project Euler Attempt
 
 This repository is dedicated for sharing my effort in solving projecteuler problems in python. The algorithm for solutions are sorted according to the archive of ([projecteuler.net](https://projecteuler.net/archives))  
