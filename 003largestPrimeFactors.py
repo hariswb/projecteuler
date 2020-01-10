d2d4e87b12004e286b3f1412b3ec3a6055525f81
hariswb <hariswb@gmail.com>
2020-01-10 20:48:21 +0700

    Problem 3 which only works with squarefree

---
+++ 003largestPrimeFactors.py
@@ -0,0 +1,16 @@
+# Problem: The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+# 
+# The algorithm only works with free square composite number such as the one the question asks 
+
+def largestPrimeFactor(num):
+    ## dividing the number until it's quotient is 1 
+    quotient = num
+    i = 1
+    while quotient > 1:
+        i += 1
+        if quotient % i == 0:
+            quotient/=i
+            print(i, quotient)
+    return(i)
+largestPrimeFactor(600851475143)
