9df907776540d3590d87d8361490221de97d2610
hariswb <hariswb@gmail.com>
2020-01-12 03:12:19 +0700

    Add solution to problem 5. The code is messy, might tidy it up later on.

---
+++ 005smallestMultiple.py
@@ -0,0 +1,43 @@
+#2520 is the smallest number that can be divided 
+# by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number 
+# that is evenly divisible by all of the numbers from 1 to 20?
+#
+# It seems like this code is overly complicated 
+# and the names are confusing lol
+
+def factorize(number):
+    num = number
+    factors = []
+    while num > 1:
+        for i in range(2, number+1):
+            if num % i == 0:
+                num /= i
+                factors.append(i)
+                break
+    return factors
+                
+def smallestMultiple(n):
+    factors = [factorize(x) for x in range(2,n+1)]
+    primes = [list(set(x)) for x in factors]
+    #print(factors, "\n",primes)
+    primePowers = []
+    result = 1
+    for primes, factors in zip(primes, factors):
+        for x in primes:
+            #print(primes, factors, [x, factors.count(x)])
+            primePowers.append([x, factors.count(x)])
+        print("{} {}".format(primes, factors))
+    primePowers.sort(key= lambda tup: tup[0])
+    #print(primePowers)
+    for i in range(2,n+1):
+        uniquePrimes = [x for x in primePowers if x[0] == i]
+        uniquePrimes.sort(key= lambda tup: tup[1])
+        #print(uniquePrimes)
+        if len(uniquePrimes) > 0:
+            largestUniquePrime = uniquePrimes[-1]
+            #print(largestUniquePrime, largestUniquePrime[0]**largestUniquePrime[1])
+            result *= (largestUniquePrime[0]**largestUniquePrime[1])
+    return result        
+
+smallestMultiple(10)
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Project Euler Attempt
 
-This repository is dedicated for sharing my effort in solving projecteuler problems in python. The algorithm for solutions are sorted according to the archive of ([projecteuler.net](https://projecteuler.net/archives))  
+This repository is dedicated for sharing my effort in solving projecteuler problems in python following freecodecamp challange [#ProjectEuler100](https://twitter.com/hashtag/ProjectEuler100?src=hashtag_click). The algorithm for solutions are sorted according to the archive of ([projecteuler.net](https://projecteuler.net/archives))  
